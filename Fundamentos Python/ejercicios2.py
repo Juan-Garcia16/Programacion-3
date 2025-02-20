e4c8dd5f7e0f00d56d9557cd227b065942f96d25
Juan Garcia <juan.garcia16@utp.edu.co>
2025-02-20 18:45:09 -0500

    avances

---
+++ Fundamentos Python/ejercicios2.py
@@ -0,0 +1,116 @@
+"""begin{question}{1}
+Para este ejercicio debe crear un programa que calcule las estaturas (\textit{interos, dobles}) promedio de un salón de clase. El usuario debe de ingresar el número cero (como una \textit{variable sentinela} de manera que pueda indicar que ya no desea ingresar mas datos)."""
+
+"""
+estaturas = []
+
+while (True):
+    estatura = float(input("Ingrese una estatura (0 para terminar de registrar): "))
+    if estatura == 0:
+        break
+    else:
+        estaturas.append(estatura)
+
+#Valida si la estructura esta vacia
+if estaturas:
+    print(f"El promedio de estaturas es: {sum(estaturas) / len(estaturas):.2f}")
+else:
+    print("No se ingresaron estaturas.")
+"""
+#-------------------------------------------------------------------------------
+"""Modifique el ejercicio anterior esta vez en lugar de digitar el número cero para indicar que se desea parar de ingresar estaturas usted debe de mostrar en pantalla el mensaje ``Desea ingresar mas estaturas [S/N]''. Utilice la respuesta obtenida para detener el ciclo."""
+'''
+estaturas = []
+
+while (True):
+    estatura = float(input("Ingrese una estatura: "))
+    estaturas.append(estatura)
+    a = input("Desea ingresar mas estaturas [S/N]: ")
+    a = a.lower()
+    
+    if a == "n":
+        break
+    elif a != "s":
+        print("Respuesta no valida, ingrese 'S' o 'N'.")
+        
+#Valida si la estructura esta vacia
+if estaturas:
+    print(f"El promedio de estaturas es: {sum(estaturas) / len(estaturas):.2f}")
+else:
+    print("No se ingresaron estaturas.")
+'''
+#-------------------------------------------------------------------------------
+'''Escriba un programa que muestre una tabla de conversión de temperatura para grados Celsius y grados Fahrenheit. La tabla debe incluir filas para todas las temperaturas entre 0 y 100 grados Celsius que sean múltiplos de 10 grados Celsius. Incluya títulos apropiados en sus columnas.'''
+
+'''
+print("\tConversion grados celsius a Fahrenheit\n")
+print("Celsius | Fahrenheit")
+print("---------------------")
+
+for i in range(11):
+    celsius = i*10
+    celsius_Fahrenheit = celsius*(9/5) + 32
+    print(celsius, "\t|", celsius_Fahrenheit)
+'''
+#-------------------------------------------------------------------------------
+'''Un auto cinema tiene un rango de precios para el ingreso a sus funciones. De 3 años o menos son admitidos sin cargo. Entre 3 y 11 años el valor de su entrada es \$16.00. Los adultos mayores de 50 años y más cuestan \$40.00. El valor de la entrada para todos los demás cuesta \$34.00.\\
+
+Usted debe de crear un programa que lea las edades de cada pasajero del automóvil y calcule el valor total de la entrada. El costo debe mostrarse usando dos decimales.'''
+
+'''
+total = 0
+
+for i in range(4):
+    pasajero = int(input(f"Ingrese edad del pasajero {i+1}: "))
+    if pasajero <= 3:
+        total += 0
+    elif pasajero > 3 and pasajero <= 11:
+        total += 16.00
+    elif pasajero > 50:
+        total += 40.00
+    else:
+        total += 34.00
+        
+print(f"El costo de la entrada al cinema para el automovil es: ${total:.2f}")
+'''
+#-------------------------------------------------------------------------------
+'''Se tiene una lista que contiene los códigos de los empleados de un almacen. Sin embargo, alguien registró erroneamente registros duplicados ['ref-010', 'ac-090','ref-010','xp-89','as-65','kj-11','xp-89', 'wx-23','rk-87','as-65']. Usted debe de escribir un programa que permita eliminar los elementos duplicados.'''
+
+'''
+codigosEmpleados = ['ref-010', 'ac-090','ref-010','xp-89','as-65','kj-11','xp-89', 'wx-23','rk-87','as-65']
+codigosEmpleadosSinDuplicado = list(set(codigosEmpleados))
+        
+print(codigosEmpleadosSinDuplicado)
+'''
+#-------------------------------------------------------------------------------
+'''Escriba un programa donde el usuario digite una palabra y obtenga una lista con las consonantes de la palabra ingresada.'''
+
+'''
+vocales = "aeiouAEIOU"
+palabra = input("Ingrese una palabra: ")
+consonantes = []
+
+for char in palabra:
+    if char.isalpha() and char not in vocales:
+        consonantes.append(char)
+
+print("Su consonantes son: ")
+print(consonantes)
+'''
+#-------------------------------------------------------------------------------
+'''Según el plan de estudios de la Universidad un alumno puede matricular al mismo tiempo Programación III y Programación IV. Escriba un programa donde:
+
+Cree una lista de asistencia por cada materia que almacene los alumnos matriculados.
+
+El programa debe de generar una lista con los alumnos que tienen matriculadas Programación III y Programación IV simultáneamente.'''
+
+'''
+programacionIII = ["Juan", "David", "Alejandro", "Paula", "Pablo", "Matias", "Esteban"]
+programacionIV = ["Carlos", "Isabella", "Martin", "Alejandra", "Alejandro", "Juan", "Paula", "Thomas"]
+
+simultaneos = list(set(programacionIII) & set(programacionIV))
+print(simultaneos)
+'''
+#-------------------------------------------------------------------------------
+'''Es conocido que los elementos de una lista pueden ser de cualquier tipo; inclusive otra lista (ej: [3.14, ``Hola Doc!'', [911, ``Los chicos malos'']]). Escriba un programa que verifique si una lista contiene una sublista.'''
+
